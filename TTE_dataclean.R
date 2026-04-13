1a777dcb33c2842611825997bea99ad5e0934889
CunniffeC <charlie.cunniffe@postgrad.manchester.ac.uk>
2026-04-13 19:30:04 +0100

    comorbidity cleaning

--- TTE_dataclean.R
+++ TTE_dataclean.R
@@ -27,8 +27,62 @@ set.seed(2602)
 
 
 ##### import data #####
-patient <- read.csv("E:/Theragnostics/Queries/Charlie/PROTECT_validation_R/data/PROTECT_patient5.csv")
+dat_protect <- read.csv("/mnt/data/charliec/ChemoRad_TTE/Data/manchester4_update.csv")%>%
+  rename(patient_id = pid)%>%
+  rename(date_seen = fup_start)%>%
+  mutate(date_seen = anydate(date_seen))
+id_dates <- dat_protect %>% select(patient_id, date_seen)
+dat_DS1 <- read.csv("/mnt/data/charliec/ChemoRad_TTE/Data/TTE_DS_data.csv") %>% mutate(date_seen = anydate(date_seen))
+dat_DS <- left_join(id_dates, dat_DS1, by = c("patient_id", "date_seen"))
+dat_patient <- read.csv("/mnt/data/charliec/ChemoRad_TTE/Data/TTE_patient_data.csv") %>% select(patient_id, imd_average_decile)
 
+TTE_final <- dat_protect %>%
+  select(patient_id, date_seen, ecog, deceased, time,  age, treatconcurrent) #weight,
 
+TTE_final <- left_join(TTE_final, dat_patient, by="patient_id")
 
+#smoking status, side 
+clean_DS <- dat_DS %>% 
+  rename(tumour_side  = side)%>%
+  select(patient_id, smoking_history, tumour_side) %>%
+  distinct()
+TTE_final <- left_join(TTE_final, clean_DS, by = "patient_id")
 
+#extract T and N stage
+dat_stage <- dat_DS %>%
+  select(patient_id, clinical_stage, path_stage) %>%
+  distinct() %>%
+  mutate(clinical_stage = ifelse(is.na(clinical_stage), path_stage, clinical_stage)) %>%
+  select(-path_stage)
+
+dat_stage$stage <- gsub("\\,.*","", dat_stage$clinical_stage)
+dat_stage$stage <- gsub("\\M.*","", dat_stage$stage)
+dat_stage$stage <- gsub("p", "", dat_stage$stage)
+dat_stage$stage <- gsub(" ", "", dat_stage$stage)
+dat_stage$stage <- gsub("T", "", dat_stage$stage)
+dat_stage <- dat_stage %>%
+  mutate(tumour_size = gsub("\\N.*","",stage))%>%
+  mutate(node_size = gsub(".*N", "",stage)) %>%
+  mutate(stage = ifelse(stage == "", NA, ifelse(stage == " ", NA, stage)))%>%
+  drop_na(stage)
+
+clean_stage <- dat_stage %>%
+  select(patient_id, tumour_size, node_size)%>%
+  distinct()
+
+TTE_final <- left_join(TTE_final, clean_stage, by = "patient_id", relationship = "many-to-many") %>%
+  distinct()
+
+#break up comorbidities
+dat_comor <- dat_DS %>%
+  select(patient_id, acecomorbidities)%>%
+  drop_na(acecomorbidities)%>%
+  distinct() %>%
+  mutate(overall_comor = ifelse(grepl("Overall", acecomorbidities, fixed = T), parse_number(acecomorbidities), NA)) %>%
+  mutate(alcohol = ifelse(grepl("Alcohol", acecomorbidities, fixed = T), parse_number(gsub(".*Alcohol", "", acecomorbidities)),NA))%>%
+  mutate(respiratory_comor = ifelse(grepl("Respiratory", acecomorbidities, fixed = T), parse_number(gsub(".*Respiratory", "", acecomorbidities)),NA))%>%
+  mutate(substance_abuse = ifelse(grepl("Drugs", acecomorbidities, fixed = T), parse_number(gsub(".*Drugs", "", acecomorbidities)),NA))%>%
+  mutate(renal_comor = ifelse(grepl("renal", acecomorbidities, fixed = T), parse_number(gsub(".*renal", "", acecomorbidities)),NA))#%>%
+#make season
+
+#check mutationrates 
